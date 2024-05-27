0f2c0b5a59ec20862f267af7071078b7a1624121
Berken Utku Demirel <54396476+Berken-demirel@users.noreply.github.com>
2024-05-27 14:36:30 +0200

    script upload

--- Heuristic_and_data_prep/WESAD/data_prep_WESAD_orig.py
+++ Heuristic_and_data_prep/WESAD/data_prep_WESAD_orig.py
@@ -29,33 +29,9 @@ for file in os.listdir():
             ppg_list.append(ppg_signal)
             ecg = data['signal']['chest']['ECG']
             ecg_list.append(ecg)
-            # imu_chest_sig = data['signal']['chest']['ACC']
-            # imu_wrist_sig = data['signal']['wrist']['ACC']
-            # total_acc = np.sqrt(imu_chest_sig[:,0]**2+imu_chest_sig[:,1]**2+imu_chest_sig[:,2]**2)
-            # imu_chest.append(imu_chest_sig[:,1])
-            # filtered_ppg = signal.sosfilt(sos, ppg_signal)
-            # segments = np.lib.stride_tricks.sliding_window_view(np.squeeze(filtered_ppg), 64*8)[::128]
-            # z_scored = stats.zscore(segments,axis=1)
-            # resampled = scipy.signal.resample(z_scored, 200, axis=1)
-            # HR = read_csv_HR(d)
-            # HR = HR.astype(float)
-            # HR_segmented = np.mean(np.lib.stride_tricks.sliding_window_view(HR, 1*8)[::2],1)
-            # ecg = data['signal']['chest']['ECG']
-            # ecg_segments = np.lib.stride_tricks.sliding_window_view(np.squeeze(ecg), 700*8)[::1400]
-            # labels = np.zeros((len(segments)))
-            # for idx in range(len(ecg_segments)):
-            #     wd, m = hp.process(ecg_segments[idx, :], 700)
-            #     if np.isnan(labels[idx]) or (idx > 3 and m['bpm'] > 25 + labels[idx-1]):
-            #         labels[idx] = np.mean(labels[idx-4:idx])
-            #     else:
-            #         labels[idx] = m['bpm']
-            # if len(np.argwhere(np.isnan(labels))) != 0:
-            #     print('NAN value')
-            # signal_list.append(resampled)
-            # label_list.append(np.expand_dims(labels,1))
 
-mdic = {"ppg_signal": ppg_list,"ecg_signal":ecg_list, 'resp_signal':resp_list}
+mdic = {"ppg_signal": ppg_list,"ecg_signal":ecg_list}
 savemat("WESAD_ppg.mat", mdic)
-dataWESAD = dict(data_ecg_avg=ecg_list, data_ppg_avg=ppg_list, data_resp_signal=resp_list)
+dataWESAD = dict(data_ecg_avg=ecg_list, data_ppg_avg=ppg_list)
 
 print('exit')
