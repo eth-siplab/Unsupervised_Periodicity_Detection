02b866b82de0936f44d5cb00daedc8e6df3fe79a
Berken Utku Demirel <54396476+Berken-demirel@users.noreply.github.com>
2024-06-03 02:22:30 -0700

    Add files via upload

---
+++ Heuristic_and_data_prep/PTB/mat_to_pkl.py
@@ -0,0 +1,9 @@
+import scipy.io
+mat = scipy.io.loadmat('ptb_v1.mat')
+data = mat['data_to_save']
+train_data = data[0,0]
+test_data = data[0,1]
+raw_data = train_data[:,0]
+bpms = train_data[:,1]
+locs = train_data[:,2]
+print('exit')
